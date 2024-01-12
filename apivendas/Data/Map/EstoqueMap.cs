d4484af4e2ffbdbeeb4405063ad0fd81a86fcd57
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2024-01-11 22:06:30 -0300

    Criado o Modelo de Estoque e seu contexto

---
+++ apivendas/Data/Map/EstoqueMap.cs
@@ -0,0 +1,21 @@
+﻿using apivendas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace apivendas.Data.Map
+{
+    public class EstoqueMap : IEntityTypeConfiguration<Estoque>
+    {
+        public void Configure(EntityTypeBuilder<Estoque> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.IdProduto).IsRequired();
+            builder.Property(e => e.Quantidade).IsRequired();
+
+            builder.HasOne(e => e.Produto)
+                .WithMany(p => p.Estoque).
+                HasForeignKey(e => e.IdProduto);
+        }
+    }
+}
---
+++ apivendas/Models/Estoque.cs
@@ -0,0 +1,11 @@
+﻿namespace apivendas.Models
+{
+    public class Estoque
+    {
+        public int Id { get; set; }
+        public int IdProduto { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto Produto { get; set; }
+    }
+}
