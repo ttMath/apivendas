2930ccae508f8d36fe2e49470c456ffa5d565cd1
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2024-01-24 10:06:42 -0300

    criado interfaces do repositório de produtos e vendas

---
+++ apivendas/Repositorios/Interfaces/IProdutoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using apivendas.Models;
+
+namespace apiprodutos.Repositorios.Interfaces
+{
+    public interface IProdutoRepositorio
+    {
+        Task<List<Produto>> Listar();
+        Task<Produto?> ListarId(int id);
+        Task<Produto> Criar(Produto produto);
+        Task<Produto> AtualizarProduto(Produto produto);       
+    }
+}
--- apivendas/Repositorios/Interfaces/IVendaRepositorio.cs
+++ apivendas/Repositorios/Interfaces/IVendaRepositorio.cs
@@ -4,9 +4,9 @@ namespace apivendas.Repositorios.Interfaces
 {
     public interface IVendaRepositorio
     {
-        Task<List<Produto>> Listar();
-        Task<Produto?> ListarId(int id);
-        Task<Produto> Criar(Produto produto);
-        Task<Produto> AtualizarProduto(Produto produto);       
+        Task<List<Venda>> Listar();
+        Task<Venda?> ListarId(int id);
+        Task<Venda> Criar(Venda venda);
+        Task<Venda> AtualizarVenda(Venda venda);       
     }
 }
