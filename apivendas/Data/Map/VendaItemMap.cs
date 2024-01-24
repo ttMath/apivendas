31273e7a25e06a84a94193cf605056a41820a1bf
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2024-01-24 09:59:04 -0300

    Criando um Map personalizado para o VendaItem

---
+++ apivendas/Data/Map/VendaItemMap.cs
@@ -0,0 +1,14 @@
+﻿using apivendas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace apivendas.Data.Map
+{
+    public class VendaItemMap : IEntityTypeConfiguration<VendaItem>
+    {
+        public void Configure(EntityTypeBuilder<VendaItem> builder)
+        {
+            builder.HasOne(x => x.Produto).WithMany().HasForeignKey(x => x.ProdutoId);
+        }
+    }
+}
