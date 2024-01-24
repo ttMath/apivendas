f641b53f1c5ecd6fdd2b33dc14aaa1bd04b85d2f
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2024-01-24 09:55:35 -0300

    Criado uma extenção do Automapper para ajudar no mapeamento dos objetos da venda.

---
+++ apivendas/Data/Map/Automapper/AutomapperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace apivendas.Data.Map.Automapper
+{
+    public static class AutomapperExtensions
+    {
+        public static T ProjectedAs<T>(this object obj) where T : class
+        {
+            if (obj is null) return null;
+            return MapperFactory.Mapper.Map<T>(obj);
+        }
+
+        public static T[] ProjectedAsCollection<T>(this object obj) where T : class
+        {
+            if (obj is null) return new T[0];
+            return MapperFactory.Mapper.Map<IEnumerable<T>>(obj)?.ToArray() ?? new T[0];
+        }
+    }
+}
---
+++ apivendas/Data/Map/Automapper/MapperFactory.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+
+namespace apivendas.Data.Map.Automapper
+{
+    public static class MapperFactory
+    {
+        static Mapper? _mapper;
+
+        static List<Profile?>? _profiles { get; set; }
+
+        public static Mapper Mapper { get { return _mapper ?? throw new Exception("Mapper Not Initialized"); } }
+
+        public static void Setup()
+        {
+            _profiles = _profiles ?? new List<Profile?>();
+
+            _profiles.Add(new ServicoParaDtos());
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.AllowNullDestinationValues = true;
+                foreach (var profile in _profiles)
+                {
+                    try
+                    {
+                        cfg.AddProfile(profile);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        throw;
+                    }
+                }
+            });
+
+            _mapper = new Mapper(config);
+        }
+    }
+}
